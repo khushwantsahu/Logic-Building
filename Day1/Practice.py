accc1442184452e005d388a5d05b2a3c658205d7
khushwantsahu <khushwantqu04@gmail.com>
2026-01-30 00:40:29 +0530

    Add files via upload

---
+++ Day1/Practice.py
@@ -0,0 +1,13 @@
+
+# DAY - 01
+#1.Program to Print Integer Numbers Entered by the User:
+integer_num = int(input("Enter integer number : "))
+print(f"you entered : {integer_num} ")
+
+#2. Program to Find the Larger Number Among Two Numbers
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+if num1>num2:
+    print(f"{num1} is > then the {num2}")
+else:
+    print(f"{num2} is > then the {num1}")
