6bb912e74abb15e450dd07af6ce858335e486caa
khushwantsahu <khushwantqu04@gmail.com>
2026-02-07 22:42:26 +0530

    Add files via upload

--- Day4/Practice.py
+++ Day4/Practice.py
@@ -1,36 +1,70 @@
 #1. Print the Multiplication Table of a Given Number
-num = int(input("Enter a number : "))
-for i in range(1,11,1):
-    print(f"{num} x {i} : {(num*i)}")
+class Table:
+    def __init__(self,num):
+        self.num = num
+    
+    def multiply(self):
+        for i in range(1,11,1):
+            print(f"{self.num} x {i} : {(self.num*i)}")
 
 #Write a Program to Make a Simple Calculator
-numA = float(input("\n\nEnter 1st number : "))
-numB = float(input("Enter 2nd number : "))
-operator = input("Enter Operator : ")
-match operator:
-    case '+':
-        print(f"{numA} + {numB} : {(numA+numB)}")
-    case '-':
-        print(f"{numA} - {numB} : {(numA-numB)}")
-    case '*':
-        print(f"{numA} * {numB} : {(numA*numB)}")
-    case '/':
-        if numB > 0:
-            print(f"{numA} / {numB} : {(numA/numB)}")
-    case '%':
-        print(f"{numA} % {numB} : {(numA%numB)}")
-    case _:
-        print(f"----Enter valid Operator----")
+class Calculator:
+    def __init__(self,numA,numB,op):
+        self.numA = numA
+        self.numB = numB 
+        self.op = op
+        
+    def select(self):
+        match self.op:
+            case '+':
+                print(f"{self.numA} + {self.numB} : {(self.numA+self.numB)}")
+            case '-':
+                print(f"{self.numA} - {self.numB} : {(self.numA-self.numB)}")
+            case '*':
+                print(f"{self.numA} * {self.numB} : {(self.numA*self.numB)}")
+            case '/':
+                if self.numB > 0:
+                    print(f"{self.numA} / {self.numB} : {(self.numA/self.numB)}")
+            case '%':
+                print(f"{self.numA} % {self.numB} : {(self.numA%self.numB)}")
+            case _:
+                print(f"----Enter valid Operator----")
     
 
 
 #3. Print a Number in Reverse Order
-numC = int(input("\n\nEnter number : "))
-sum = 0
-n = numC
-while(n>0):
-    rem = int(n%10)
-    sum = int(10*sum + rem)
-    n = n//10
+class Reverse:
+    def __init__(self,num):
+        self.num = num 
 
-print(f"reverse of {numC} : {sum}")
+    def reverse(self):
+        sum = 0
+        n = self.num
+        while(n>0):
+            rem = int(n%10)
+            sum = int(10*sum + rem)
+            n = n//10
+        print(f"reverse of {self.num} : {sum}")
+
+def main():
+    num = float(input("\nEnter number : "))
+    table = Table(num=num)
+    table.multiply()
+
+
+    while True:
+        num1 = float(input("\n\nEnter number : "))
+        num2 = float(input("Enter number : "))
+        op = input("Enter Operator : ")
+        calculator  = Calculator(numA= num1,op=op,numB=num2)
+        calculator.select()
+
+        option = input("Want to continue(Y/N) : ")
+        if option.lower() == 'n':
+            break
+    
+    num = int(input("\nEnter number to reverse : "))
+    reverse = Reverse(num=num)
+    reverse.reverse()
+
+main()
