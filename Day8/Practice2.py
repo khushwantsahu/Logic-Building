bf6367f3eda89cbdb89e0e9401878aaad62bd863
khushwantsahu <khushwantqu04@gmail.com>
2026-02-13 22:31:19 +0530

    Adding Match Case

--- Day8/Practice2.py
+++ Day8/Practice2.py
@@ -12,12 +12,35 @@ int_to_word(string_num)
 
 #5.  Print Numbers in Words in Reverse Order Using a Switch Case:
 def int_to_word_in_rev(string):
+    '''
     dict = {"1":"One","2":"Two","3":"Three","4":"Four","5":"Five","6":"Six","7":"Seven","8":"Eight","9":"Nine","0":"Zero"}
     n = len(string)
     for i in range(1,n+1):
         digit = string[-i]
         print(f"{dict[digit]}",end=" ")
-
+    '''
+    for ch in string[::-1]:
+        match ch:
+            case "1":
+                print("One",end=" ")
+            case "2":
+                print("Two",end=" ")
+            case "3":
+                print("Three",end=" ")
+            case "4":
+                print("Four",end=" ")
+            case "5":
+                print("Five",end=" ")
+            case "6":
+                print("Six",end=" ")
+            case "7":
+                print("Seven",end=" ")
+            case "8":
+                print("Eight",end=" ")
+            case "9":
+                print("Nine",end=" ")
+            case _:
+                print("Enter Only Integer")
 
 str_num = input("\n\nEnter a number : ")
 int_to_word_in_rev(str_num)
@@ -54,4 +77,5 @@ def armstrong(num):
 
 
 num = input("\n\nEnter a num : ")
+
 armstrong(num)
